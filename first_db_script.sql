e27b2d4cf218f18b3f990f21318c4a8c24c67600
Tyson <tyson@Tysons-MBP.hitronhub.home>
2022-03-02 21:41:05 -0500

    added database script

---
+++ first_db_script.sql
@@ -0,0 +1,26 @@
+-- USE first_db;
+-- CREATE schema first_db;
+
+-- CREATE table users (
+-- user_id INT AUTO_INCREMENT,
+-- first_name VARCHAR(255),
+-- last_name VARCHAR(255),
+-- created_on VARCHAR(255),
+-- PRIMARY KEY(user_id)
+-- );
+
+-- CREATE table purchases (
+-- 	   order_id int NOT NULL AUTO_INCREMENT,    # CONSTRAINTS
+--     user_id int NOT NULL,
+--     purchase_date VARCHAR(255),
+--     price double,
+--     PRIMARY KEY (order_id),
+--     FOREIGN KEY (user_id) REFERENCES users(user_id)
+--     );
+
+-- INSERT INTO users (first_name, last_name, created_on)
+-- VALUES ('Tyson', 'Chea', '2003-06-06');
+
+-- INSERT INTO purchases (user_id, purchase_date, price)
+-- VALUES (1, 2022-03-02, 10.99)
+
