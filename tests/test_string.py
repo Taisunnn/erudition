80826306624f8cdb3b0f6cc0dd449deeab5a0ab2
Tyson <tyson@tysons-mbp.wifi.int.ryerson.ca>
2022-03-11 10:47:17 -0500

    testing

--- tests/test_string.py
+++ tests/test_string.py
@@ -11,4 +11,4 @@ def test_string(input_string2):
     assert reverse_string(input_string2) == 'god'
 
 def test_string(input_string3):
-    assert reverse_string(input_string3) == '  dlrow  olleh  '
+    assert reverse_string(input_string3) == '  dlrow olleh  '
